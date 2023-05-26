42185a826bd4345cf05e9cf6f3311ba8cccebbf0
Charlotte Ellwood <char502@hotmail.com>
2023-05-26 11:02:27 +0100

    finished test

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
-import Home from "../pages/index";
-
-// import data from "../data.json";
-// import prosAndConsObj from "./index";
+import Article from "../pages/index";
 
 describe("HomePage", () => {
   it("should render index page", async () => {
-    render(<Home />);
+    render(<Article />);
 
     await waitFor(() => {
       screen.findByText("Samsung galaxy A53");
+      screen.findByText("32645");
       screen.findByText("Stunning screen");
-
-      screen.logTestingPlaygroundURL();
+      screen.findByText("Quality cameras");
+      screen.findByText("Five years of security updates");
+      screen.findByText("No wireless charging");
+      screen.findByText("Average performance");
     });
   });
 });
